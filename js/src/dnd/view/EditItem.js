9be6f30c70348b9e2f733b0c2f4d77f118df61a1
Ray <rwadkins@twors.net>
2013-05-04 18:38:02 -0400

    Edit Item View

---
+++ js/src/dnd/view/EditItem.js
@@ -0,0 +1,15 @@
+define([
+    "dojo/_base/declare", 
+    "dijit/_WidgetBase", 
+    "dijit/_TemplatedMixin", 
+    "dojo/text!dnd/view/EditItem/template.html"
+], function(
+    declare, 
+    _WidgetBase, 
+    _TemplatedMixin, 
+    template
+) {
+   return declare([_WidgetBase, _TemplatedMixin], {
+      templateString: template,
+   });
+});
