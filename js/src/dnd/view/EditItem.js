3f06556b7bf0873afa4a57aa50bf0746ee427c96
Ray <rwadkins@twors.net>
2013-05-11 12:33:21 -0400

    More Edit Item Styling.  yay flexbox

--- js/src/dnd/view/EditItem.js
+++ js/src/dnd/view/EditItem.js
@@ -3,38 +3,39 @@ define([
     "dijit/_WidgetBase", 
     "dijit/_TemplatedMixin", 
     "dojo/topic", 
-    "dojo/_base/lang",
-    "dojo/dom-class",
-    "dojo/on",
+    "dojo/_base/lang", 
+    "dojo/on", 
     "dojo/text!dnd/view/EditItem/template.html"
 ], function(
     declare, 
     _WidgetBase, 
     _TemplatedMixin, 
     topic, 
-    lang,
-    domClass,
-    on,
+    lang, 
+    on, 
     template
 ) {
-   return declare([_WidgetBase, _TemplatedMixin], {
-      templateString: template,
-      startup: function() {
-          console.log("EditItem startup");
-          this.inherited(arguments);
-          
-          topic.subscribe("editItem", lang.hitch(this,"editItemSubscribe"));
-          
-          this.own(
-              on(this.domNode, "button:click", lang.hitch(this, "buttonClick")) 
-          );
-      },
-      editItemSubscribe: function() {
-          console.log("editItemSubscribe");
-          domClass.add(this.domNode, "visible");
-      },
-      buttonClick: function(e) {
-          domClass.remove(this.domNode, "visible");  
-      }
-   });
-});
+    return declare([_WidgetBase, _TemplatedMixin], {
+        templateString : template,
+        startup : function() {
+            this.inherited(arguments);
+
+            this.own(
+                on(this.domNode, "button:click", lang.hitch(this, "buttonClick")),
+                on(this.domNode, "backbutton", lang.hitch(this,"backButton"))
+            );
+        },
+        buttonClick : function(e) {
+            this.hide();
+        },
+        keyPress : function(e) {
+            
+        },
+        backButton : function(e) {
+            this.hide();
+        },
+        hide : function() {
+            topic.publish("editItemDone", null);
+        }
+    });
+}); 
--- js/src/dnd/view/Main.js
+++ js/src/dnd/view/Main.js
@@ -2,12 +2,18 @@ define([
     "dojo/_base/declare", 
     "dijit/_WidgetBase", 
     "dijit/_TemplatedMixin",
+    "dojo/topic",
+    "dojo/_base/lang",
+    "dojo/dom-class",
     "dnd/view/EditItem",  
     "dojo/text!dnd/view/Main/template.html"
 ], function(
     declare, 
     _WidgetBase, 
     _TemplatedMixin,
+    topic,
+    lang,
+    domClass,
     EditItem, 
     template
 ) {
@@ -18,6 +24,17 @@ define([
           this.inherited(arguments);
           var editItem = new EditItem({}, this.itemEditNode);
           editItem.startup();
+      },
+      startup: function() {
+          topic.subscribe("editItem", lang.hitch(this,"editItemSubscribe"));
+          topic.subscribe("editItemDone", lang.hitch(this,"editItemDoneSubscribe"));
+      },
+      editItemSubscribe: function(e) {
+          console.log("editItemSubscribe");
+          domClass.add(this.domNode, "editItem");
+      },
+      editItemDoneSubscribe: function(e) {
+          domClass.remove(this.domNode, "editItem");
       }
    });
 });
